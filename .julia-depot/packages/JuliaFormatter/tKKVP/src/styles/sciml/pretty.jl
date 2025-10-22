b7e87c00e485bf0d9236409c5ed0ec28138e0ece
kanji709 <70739077+kanji709@users.noreply.github.com>
2025-10-22 13:14:17 +0000

    Format .jl files

---
+++ .julia-depot/packages/JuliaFormatter/tKKVP/src/styles/sciml/pretty.jl
@@ -0,0 +1,206 @@
+struct SciMLStyle <: AbstractStyle
+    innerstyle::AbstractStyle
+end
+SciMLStyle() = SciMLStyle(NoopStyle())
+getstyle(s::SciMLStyle) = s.innerstyle isa NoopStyle ? s : s.innerstyle
+
+function options(::SciMLStyle)
+    return (;
+        always_for_in = true,
+        always_use_return = false,
+        annotate_untyped_fields_with_any = true,
+        conditional_to_if = false,
+        import_to_using = false,
+        join_lines_based_on_source = true,
+        normalize_line_endings = "unix",
+        pipe_to_function_call = false,
+        remove_extra_newlines = true,
+        short_to_long_function_def = true,
+        long_to_short_function_def = false,
+        whitespace_in_kwargs = true,
+        whitespace_ops_in_indices = true,
+        whitespace_typedefs = true,
+        indent = 4,
+        margin = 92,
+        format_docstrings = false,
+        align_struct_field = false,
+        align_assignment = false,
+        align_conditional = false,
+        align_pair_arrow = false,
+        align_matrix = false,
+        trailing_comma = false,
+        trailing_zero = true,
+        indent_submodule = false,
+        separate_kwargs_with_semicolon = false,
+        surround_whereop_typeparameters = true,
+        variable_call_indent = [],
+        yas_style_nesting = false,
+        disallow_single_arg_nesting = true,
+    )
+end
+
+function is_binaryop_nestable(::SciMLStyle, cst::JuliaSyntax.GreenNode)
+    if (defines_function(cst) || is_assignment(cst))
+        return false
+    else
+        false
+    end
+    !(op_kind(cst) in KSet"=> -> in")
+end
+@doc """
+    SciMLStyle()
+
+Formatting style based on [SciMLStyle](https://github.com/SciML/SciMLStyle).
+
+Configurable options with different defaults to [`DefaultStyle`](@ref) are:
+$(list_different_defaults(SciMLStyle()))
+"""
+SciMLStyle
+
+const CST_T = [JuliaSyntax.GreenNode]
+const TUPLE_T = [JuliaSyntax.GreenNode, Vector{JuliaSyntax.GreenNode}]
+for f in [
+    :p_import,
+    :p_using,
+    :p_export,
+    :p_public,
+    :p_vcat,
+    :p_ncat,
+    :p_typedvcat,
+    :p_typedncat,
+    :p_row,
+    :p_nrow,
+    :p_hcat,
+    :p_comprehension,
+    :p_typedcomprehension,
+    :p_generator,
+    :p_filter,
+]
+    @eval function $f(
+        ss::SciMLStyle,
+        cst::JuliaSyntax.GreenNode,
+        s::State,
+        ctx::PrettyContext,
+        lineage::Vector{Tuple{JuliaSyntax.Kind,Bool,Bool}},
+    )
+        $f(YASStyle(getstyle(ss)), cst, s, ctx, lineage)
+    end
+end
+
+for f in [
+    :p_call,
+    :p_curly,
+    :p_ref,
+    :p_braces,
+    # :p_vect, don't use YAS style vector formatting with `yas_style_nesting = true`
+    :p_parameters,
+    :p_invisbrackets,
+    :p_bracescat,
+]
+    @eval function $f(
+        ss::SciMLStyle,
+        cst::JuliaSyntax.GreenNode,
+        s::State,
+        ctx::PrettyContext,
+        lineage::Vector{Tuple{JuliaSyntax.Kind,Bool,Bool}},
+    )
+        if s.opts.yas_style_nesting
+            $f(YASStyle(getstyle(ss)), cst, s, ctx, lineage)
+        else
+            $f(DefaultStyle(getstyle(ss)), cst, s, ctx, lineage)
+        end
+    end
+end
+
+function p_tuple(
+    ss::SciMLStyle,
+    cst::JuliaSyntax.GreenNode,
+    s::State,
+    ctx::PrettyContext,
+    lineage::Vector{Tuple{JuliaSyntax.Kind,Bool,Bool}},
+)
+    if s.opts.yas_style_nesting
+        p_tuple(YASStyle(getstyle(ss)), cst, s, ctx, lineage)
+    else
+        p_tuple(DefaultStyle(getstyle(ss)), cst, s, ctx, lineage)
+    end
+end
+
+function p_macrocall(
+    ss::SciMLStyle,
+    cst::JuliaSyntax.GreenNode,
+    s::State,
+    ctx::PrettyContext,
+    lineage::Vector{Tuple{JuliaSyntax.Kind,Bool,Bool}},
+)
+    style = getstyle(ss)
+    t = FST(MacroCall, nspaces(s))
+    if !haschildren(cst)
+        return t
+    end
+
+    childs = children(cst)
+    has_closer = is_closer(childs[end])
+    is_macroblock = !has_closer
+
+    if is_macroblock
+        t.typ = MacroBlock
+    end
+
+    idx = findfirst(a -> kind(a) === K"(", childs)
+    first_arg_idx =
+        idx === nothing ? -1 : findnext(a -> !JuliaSyntax.is_whitespace(a), childs, idx + 1)
+
+    # https://github.com/SciML/SciMLStyle?tab=readme-ov-file#macros
+    n_kw_args = count(a -> kind(a) === K"=" && haschildren(a), childs)
+    nospace = n_kw_args > 1
+
+    for (i, a) in enumerate(childs)
+        n = pretty(
+            style,
+            a,
+            s,
+            newctx(ctx; nospace = nospace, can_separate_kwargs = false),
+            lineage,
+        )::FST
+
+        override = (i == first_arg_idx) || kind(a) === K")"
+
+        if JuliaSyntax.is_macro_name(a) || kind(a) === K"("
+            add_node!(t, n, s; join_lines = true)
+        elseif kind(a) === K","
+            add_node!(t, n, s; join_lines = true)
+            if needs_placeholder(childs, i + 1, K")")
+                add_node!(t, Placeholder(1), s)
+            end
+        elseif JuliaSyntax.is_whitespace(a)
+            add_node!(t, n, s; join_lines = true)
+        elseif is_macroblock
+            if n.typ === MacroBlock && t[end].typ === WHITESPACE
+                t[end] = Placeholder(length(t[end].val))
+            end
+
+            max_padding = is_block(n) ? 0 : -1
+            join_lines = t.endline == n.startline
+
+            if join_lines && (i > 1 && kind(childs[i-1]) in KSet"NewlineWs Whitespace") ||
+               next_node_is(nn -> kind(nn) in KSet"NewlineWs Whitespace", childs[i])
+                add_node!(t, Whitespace(1), s)
+            end
+            add_node!(t, n, s; join_lines, max_padding)
+        else
+            add_node!(
+                t,
+                n,
+                s;
+                join_lines = true,
+                override_join_lines_based_on_source = override,
+            )
+        end
+    end
+    # move placement of @ to the end
+    #
+    # @Module.macro -> Module.@macro
+    t[1] = move_at_sign_to_the_end(t[1], s)
+    t
+end
--- test/gbs.jl
+++ test/gbs.jl
@@ -30,7 +30,7 @@ function sampleSNPPosition(totalSNP::Int64, winSize::Int64, mu::Float64, sigmasq
             alpha = mu / theta # calcualte Gamma paramter: shape
             density = rand(Gamma(alpha, theta), numWin) # sample SNP density for each window
             win = fill(winSize, numWin) # generate a vector (with length = numWin) contating windown size for each window
-            winPos = [0; cumsum(win)[1:end-1]] # starting postion of each window
+            winPos = [0; cumsum(win)[1:(end-1)]] # starting postion of each window
             sam = [rand(Bernoulli(density[i]), winSize) for i = 1:numWin] # sampling the occurence of SNP at each bp using local SNP density (sampled from Gamma model for each win)
             sam2 = map(x -> findall(x .> 0), sam) # record SNP positions within each window
             sam3 = reduce(vcat, [sam2[i] .+ winPos[i] for i = 1:numWin]) # calcualte SNP position within chromosome
@@ -157,12 +157,12 @@ function getHaplotypes(samples = ind)
     ends = totalSNP .+ 1
     haplotypes = Array{Int64}(undef, 2 * size(samples, 1), sum(numSNP) + 1)
     for i = 1:numInd
-        haplotypes[2*i-1:2*i, 1] .= samples[i].ID
+        haplotypes[(2*i-1):(2*i), 1] .= samples[i].ID
         for c = 1:numChr
             mat = samples[i].MatChrs[c].SNPs
             pat = samples[i].PatChrs[c].SNPs
-            haplotypes[collect(1:2:2*numInd)[i], starts[c]:ends[c]] = mat
-            haplotypes[collect(2:2:2*numInd)[i], starts[c]:ends[c]] = pat
+            haplotypes[collect(1:2:(2*numInd))[i], starts[c]:ends[c]] = mat
+            haplotypes[collect(2:2:(2*numInd))[i], starts[c]:ends[c]] = pat
         end
     end
     haplotypes
@@ -352,7 +352,7 @@ function GBS(
                         seg[length(sites)+1]
                         re[1].overhang[1]
                     ],
-                )[1:stops] for i = 1:2:numInd*2
+                )[1:stops] for i = 1:2:(numInd*2)
             ] # genearte GBS reads from martenal haplotypes
             patHap = [
                 join(
@@ -361,7 +361,7 @@ function GBS(
                         seg[length(sites)+1]
                         re[1].overhang[1]
                     ],
-                )[1:stops] for i = 2:2:numInd*2
+                )[1:stops] for i = 2:2:(numInd*2)
             ] # generate GBS reads from partenal haplotypes
             matHapRevComp =
                 [reverse(map(x -> comp[x], matHap[i]))[1:stops] for i = 1:numInd] # reverse complement GBS reads of maternal haplotypes
