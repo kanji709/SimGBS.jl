ef2b758f8220fdf48dc5b2cac573994bf88f5046
Max Horn <max@quendi.de>
2025-10-27 09:32:48 +0100

    Remove Julia depot from repository

--- .julia-depot/packages/CommonMark/lmLkP/.ci/changelog.jl
+++
@@ -1,9 +0,0 @@
-import Changelog
-
-cd(dirname(@__DIR__)) do
-    Changelog.generate(
-        Changelog.CommonMark(),
-        "CHANGELOG.md";
-        repo = "MichaelHatherly/CommonMark.jl",
-    )
-end
